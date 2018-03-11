0a6da43a903fd2df8f8c3d82da72519bf7ec698f
Sanjeev K Panicker <sanjeevk.panicker@gmail.com>
2018-03-11 18:06:46 -0400

    First

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const nedb = require('nedb');
+const rest = require('express-nedb-rest');
+const cors = require('cors');
+
+const app = express();
+
+const datastore = new nedb({ filename: 'mycoffee.db', autoload: true });
+
+const restAPI = rest();
+restAPI.addDatastore('coffees', datastore);
+
+app.use(cors());
+app.use('/', restAPI);
+
+app.listen(3102);
